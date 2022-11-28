df46b6282c0f856f9d78a04610a9831c8b4c3af8
Brent Lasaca <brent.lasaca@cit.edu>
2022-11-28 17:40:21 +0800

    Create IStringChecker interface

---
+++ PDA Programming Assignment/PDA Programming Assignment/IStringChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Programming_Assignment
+{
+    public interface IStringChecker
+    {
+        void CheckString(string input);
+    }
+}
