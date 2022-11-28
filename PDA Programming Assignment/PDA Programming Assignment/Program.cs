c973172d90f96cd1f62a806198f93d288c40cad4
Brent Lasaca <brent.lasaca@cit.edu>
2022-11-28 17:41:54 +0800

    Implement Program workflow

---
+++ PDA Programming Assignment/PDA Programming Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using PDA_Programming_Assignment;
+
+Console.Write("Enter string: ");
+string input = Console.ReadLine();
+Console.Write("String checker to use (P - Palindrome, B - Balanced Symbol): ");
+string checker = Console.ReadLine();
+
+StringInput str;
+if (checker.Equals("P")){
+    str = new StringInput(new PalindromeChecker());
+    str.CheckString(input);
+}
+else if (checker.Equals("B"))
+{
+    str = new StringInput(new BalancedSymbolChecker());
+    str.CheckString(input);
+}
+else
+{
+    throw new ArgumentException();
+}
