26c4824c207a2aa5c0e62812fdfc46780b941fe0
Brent Lasaca <brent.lasaca@cit.edu>
2022-11-28 17:41:45 +0800

    Create StringInput class that has a composition with IStringChecker

---
+++ PDA Programming Assignment/PDA Programming Assignment/StringInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Programming_Assignment
+{
+    public class StringInput
+    {
+        public IStringChecker _stringChecker;
+
+        public StringInput(IStringChecker stringChecker)
+        {
+            _stringChecker = stringChecker;
+        }
+
+        public void CheckString(string input)
+        {
+            _stringChecker.CheckString(input);
+        }
+    }
+}
