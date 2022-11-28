02251c4c2d2e5f22e331012306fc12c2902d79e0
Brent Lasaca <brent.lasaca@cit.edu>
2022-11-28 17:40:51 +0800

    Create PalindromeChecker class to implement interface

---
+++ PDA Programming Assignment/PDA Programming Assignment/PalindromeChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Programming_Assignment
+{
+    public class PalindromeChecker : IStringChecker
+    {
+        public void CheckString(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+            int length = input.Length;
+            int i, mid = length / 2;
+
+            for (i = 0; i < mid; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            if (length % 2 != 0)
+            {
+                i++;
+            }
+
+            while (i < length)
+            {
+                char element = stack.Pop();
+                if (element != input[i])
+                {
+                    Console.WriteLine("The inputted string is not a palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine("The inputted string is a palindrome.");
+                }
+                i++;
+            }
+        }
+    }
+}
