9901e620dd12fa6e176bf958c36f60b7e0c930a2
Brent Lasaca <brent.lasaca@cit.edu>
2022-11-28 17:41:04 +0800

    Create BalancedSymbolChecker class to implement interface

---
+++ PDA Programming Assignment/PDA Programming Assignment/BalancedSymbolChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Programming_Assignment
+{
+    public class BalancedSymbolChecker : IStringChecker
+    {
+        public void CheckString(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+            int length = input.Length;
+            int stringNumber = 0;
+            
+            for(int i = 0; i < length; i++)
+            {
+                if(input[i].Equals('(') || input[i].Equals('{') || input[i].Equals('['))
+                {
+                    stack.Push(input[i]);
+                }
+                else if(input[i].Equals(')') || input[i].Equals('}') || input[i].Equals(']'))
+                {
+                    if (stack.Count == 0)
+                    {
+                        stringNumber++;
+                        Console.WriteLine("The inputted string is not balanced.");
+                    }
+                    else
+                    {
+                        stack.Pop();
+                        
+                    }
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                Console.WriteLine("The inputted string is not balanced.");
+            }
+            else if (stack.Count == 0 && stringNumber == 0)
+            {
+                Console.WriteLine("The inputted string is balanced.");
+            }
+            
+        }
+    }
+}
